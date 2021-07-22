ubuntudesign/www.ubuntu.com

Add invoice page

webapp/advantage/views.py
@@ -1,4 +1,5 @@
 # Standard library
+import urllib.request
 from datetime import datetime, timedelta, timezone
 
 # Packages
@@ -29,10 +30,26 @@ from webapp.advantage.schemas import (
     post_customer_info,
     ensure_purchase_account,
     post_payment_method,
+    invoice_view,
 )
 
 
 session = talisker.requests.get_session()
+
+services = {
+    "canonical-ua": {
+        "short": "ua",
+        "name": "Canonical UA",
+    },
+    "canonical-blender": {
+        "short": "blender",
+        "name": "Blender Support",
+    },
+    "canonical-cube": {
+        "short": "cube",
+        "name": "Canonical CUBE",
+    },
+}
 
 
 @advantage_checks(check_list=["is_maintenance", "view_need_user"])
@@ -745,6 +762,117 @@ def account_view(**kwargs):
         "account/index.html",
         email=email,
     )
+
+
+@advantage_checks(check_list=["is_maintenance", "view_need_user"])
+@use_kwargs(invoice_view, location="query")
+def invoices_view(**kwargs):
+    is_test_backend = kwargs.get("test_backend")
+    token = kwargs.get("token")
+    api_url = kwargs.get("api_url")
+    email = kwargs.get("email", "").strip()
+    marketplace = kwargs.get("marketplace")
+
+    advantage = UAContractsAPI(
+        session, token, api_url=api_url, is_for_view=True
+    )
+
+    accounts = advantage.get_accounts(email=email)
+
+    total_payments = []
+    for account in accounts:
+        raw_payments = advantage.get_account_purchases(
+            account_id=account["id"],
+            filters={"marketplace": marketplace},
+        )
+
+        if not raw_payments:
+            continue
+
+        product_listings = raw_payments["productListings"]
+        for raw_payment in raw_payments["purchases"]:
+            payment_id = raw_payment["id"]
+            payment_marketplace = raw_payment["marketplace"]
+            created_at = raw_payment["createdAt"]
+            total = raw_payment["invoice"]["total"] / 100
+            currency = raw_payment["invoice"]["currency"]
+            listing_id = raw_payment["purchaseItems"][0]["productListingID"]
+
+            period = None
+            for product_listing in product_listings:
+                if product_listing["id"] == listing_id:
+                    period = product_listing["period"]
+
+                    break
+
+            file_name = _get_download_file_name(raw_payment, period)
+            download_link = f"invoices/download/{period}/{payment_id}"
+
+            total_payments.append(
+                {
+                    "created_at": created_at,
+                    "service": services[payment_marketplace]["name"],
+                    "period": "Monthly" if period == "monthly" else "Annual",
+                    "date": parse(created_at).strftime("%d %B, %Y"),
+                    "total": f"{total} {currency}",
+                    "download_file_name": file_name,
+                    "download_link": download_link,
+                }
+            )
+
+    total_payments.sort(key=lambda item: item["created_at"], reverse=True)
+
+    return flask.render_template(
+        "account/invoices/index.html",
+        invoices=total_payments,
+        marketplace=marketplace,
+        is_test_backend=is_test_backend,
+    )
+
+
+@advantage_checks(check_list=["is_maintenance", "view_need_user"])
+def download_invoice(period, purchase_id, **kwargs):
+    token = kwargs.get("token")
+    api_url = kwargs.get("api_url")
+
+    advantage = UAContractsAPI(
+        session, token, api_url=api_url, is_for_view=True
+    )
+
+    purchase = advantage.get_purchase(purchase_id)
+    file_name = _get_download_file_name(purchase, period)
+    download_link = purchase["invoice"]["url"]
+
+    response = urllib.request.urlopen(download_link)
+
+    return flask.Response(
+        response.read(),
+        mimetype="application/pdf",
+        headers={"Content-Disposition": f"attachment;filename={file_name}"},
+    )
+
+
+def _get_download_file_name(payment, period):
+    payment_marketplace = payment["marketplace"]
+    created_at = payment["createdAt"]
+
+    file_name_elements = [services[payment_marketplace]["short"]]
+
+    if period == "monthly" or payment_marketplace == "canonical-cube":
+        formatted_date = parse(created_at).strftime("%d%b%y").lower()
+        file_name_elements.append(formatted_date)
+
+    if period == "yearly":
+        formatted_current_date = parse(created_at).strftime("%y")
+        formatted_next_date = str(int(formatted_current_date) + 1)
+
+        file_name_elements.append("annual")
+        file_name_elements.append(formatted_current_date)
+        file_name_elements.append(formatted_next_date)
+
+    file_name = "-".join(file_name_elements)
+
+    return f"{file_name}.pdf"
 
 
 def _prepare_monthly_info(monthly_info, subscription, advantage):

albertkol/ubuntu.com

webapp/advantage/views.py
@@ -1,4 +1,5 @@
 # Standard library
+import urllib.request
 from datetime import datetime, timedelta, timezone
 
 # Packages
@@ -29,10 +30,26 @@ from webapp.advantage.schemas import (
     post_customer_info,
     ensure_purchase_account,
     post_payment_method,
+    invoice_view,
 )
 
 
 session = talisker.requests.get_session()
+
+SERVICES = {
+    "canonical-ua": {
+        "short": "ua",
+        "name": "Canonical UA",
+    },
+    "canonical-blender": {
+        "short": "blender",
+        "name": "Blender Support",
+    },
+    "canonical-cube": {
+        "short": "cube",
+        "name": "Canonical CUBE",
+    },
+}
 
 
 @advantage_checks(check_list=["is_maintenance", "view_need_user"])
@@ -745,6 +762,124 @@ def account_view(**kwargs):
         "account/index.html",
         email=email,
     )
+
+
+@advantage_checks(check_list=["is_maintenance", "view_need_user"])
+@use_kwargs(invoice_view, location="query")
+def invoices_view(**kwargs):
+    is_test_backend = kwargs.get("test_backend")
+    token = kwargs.get("token")
+    api_url = kwargs.get("api_url")
+    email = kwargs.get("email", "").strip()
+    marketplace = kwargs.get("marketplace")
+
+    advantage = UAContractsAPI(
+        session, token, api_url=api_url, is_for_view=True
+    )
+
+    accounts = advantage.get_accounts(email=email)
+
+    total_payments = []
+    for account in accounts:
+        raw_payments = advantage.get_account_purchases(
+            account_id=account["id"],
+            filters={"marketplace": marketplace},
+        )
+
+        if not raw_payments:
+            continue
+
+        product_listings = raw_payments["productListings"]
+        for raw_payment in raw_payments["purchases"]:
+            payment_id = raw_payment["id"]
+            payment_marketplace = raw_payment["marketplace"]
+            created_at = raw_payment["createdAt"]
+
+            total = "-"
+            if raw_payment.get("invoice"):
+                cost = raw_payment["invoice"]["total"] / 100
+                currency = raw_payment["invoice"]["currency"]
+                total = f"{cost} {currency}"
+
+            listing_id = raw_payment["purchaseItems"][0]["productListingID"]
+            period = None
+            for product_listing in product_listings:
+                if product_listing["id"] == listing_id:
+                    period = product_listing["period"]
+
+                    break
+
+            file_name = ""
+            download_link = ""
+            if raw_payment.get("invoice"):
+                file_name = _get_download_file_name(raw_payment, period)
+                download_link = f"invoices/download/{period}/{payment_id}"
+
+            total_payments.append(
+                {
+                    "created_at": created_at,
+                    "service": SERVICES[payment_marketplace]["name"],
+                    "period": "Monthly" if period == "monthly" else "Annual",
+                    "date": parse(created_at).strftime("%d %B, %Y"),
+                    "total": total,
+                    "download_file_name": file_name,
+                    "download_link": download_link,
+                }
+            )
+
+    total_payments.sort(key=lambda item: item["created_at"], reverse=True)
+
+    return flask.render_template(
+        "account/invoices/index.html",
+        invoices=total_payments,
+        marketplace=marketplace,
+        is_test_backend=is_test_backend,
+    )
+
+
+@advantage_checks(check_list=["is_maintenance", "view_need_user"])
+def download_invoice(period, purchase_id, **kwargs):
+    token = kwargs.get("token")
+    api_url = kwargs.get("api_url")
+
+    advantage = UAContractsAPI(
+        session, token, api_url=api_url, is_for_view=True
+    )
+
+    purchase = advantage.get_purchase(purchase_id)
+    file_name = _get_download_file_name(purchase, period)
+    download_link = purchase["invoice"]["url"]
+
+    response = urllib.request.urlopen(download_link)
+
+    return flask.Response(
+        response.read(),
+        mimetype="application/pdf",
+        headers={"Content-Disposition": f"attachment;filename={file_name}"},
+    )
+
+
+def _get_download_file_name(payment, period):
+    payment_marketplace = payment["marketplace"]
+    created_at = payment["createdAt"]
+
+    file_name_elements = [SERVICES[payment_marketplace]["short"]]
+
+    if period == "monthly" or payment_marketplace == "canonical-cube":
+        formatted_date = parse(created_at).strftime("%d%b%y").lower()
+        file_name_elements.append(formatted_date)
+
+    if period == "yearly":
+        formatted_current_date = parse(created_at).strftime("%y")
+        formatted_next_date = str(int(formatted_current_date) + 1)
+
+        file_name_elements.append("annual")
+        file_name_elements.append(formatted_current_date)
+        file_name_elements.append(formatted_next_date)
+
+    file_name = "-".join(file_name_elements)
+
+    return f"{file_name}.pdf"
 
 
 def _prepare_monthly_info(monthly_info, subscription, advantage):